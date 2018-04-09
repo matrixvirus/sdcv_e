26a1d511be942dc1fe6bfb44d644b877693136d7
彭一航 <matrixvirus@163.com>
2018-04-09 18:44:47 -0400

    见readme

---
+++ sdcv_e.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+function interactMode()
+{
+# 交互模式，循环查词，当命令没有跟参数时使用。
+	echo "记住查qqq这个单词启以退出我"
+	local word=""
+	while [ "$word" != "qqq" ]
+	do
+		read -p "请输入单词: " word
+		if [ "$word" = "qqq" ]
+		then
+			break
+		fi
+		sdcv "$word" -n | less
+		# sdcv 的-n参数意思是不交互，比如你查qq，这个单词并不存在，正常情况
+		# sdcv 会列出各个字典中与qq接近的词，并允许你通过输入数字来选择要查
+		# 看哪个与qq接近的词的详细解释，加了-n后，sdcv直接列出所有字典中与qq
+		# 接近的词的解释，不再提供选择。
+	done
+	echo -e "\e[5;36m谢谢使用\e[0m"
+	# \e[5;36m 意思是使用莹蓝色，闪烁字，5是闪烁，36是莹蓝色。
+	
+}
+function staticMode()
+{
+# 只查命令参数里指定的几个单词，然后退出。
+	local word=""
+	while [ $# -gt 0 ]
+	do
+		word=$1
+		sdcv "$word" -n | less
+		shift
+	done
+}
+case $# in
+0)
+	interactMode
+	;;
+*)
+	staticMode $@
+	;;	
+esac
